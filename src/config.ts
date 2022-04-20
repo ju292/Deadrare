8fae5f3d27ea0a650590553d579235ed7bb85fda
ju292 <102867147+ju292@users.noreply.github.com>
2022-04-20 23:41:07 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -11,7 +11,7 @@ export const NFTContract = 'erd1qqqqqqqqqqqqqpgq32payvmle5ewfaqv9kgyct7medxlwazp
 export const NFTName = 'Elrond Apes #2582';
 export const NFTRank = '9818';              //Rank :
 export const NFTScore ='67';                // Score:
-export const price = 0.01;    // IN EGLD
+export const price = 2;    // IN EGLD
 export const NFTMediaLink = 'https://media.elrond.com/nfts/asset/QmRP9FVznm1xxa3FEsDENXhafRDaRWY6LKQdg8GygHNHKw/2582.png';             // the link for the image (media.elrond.etc)
 
 // ATTRIBUTES
