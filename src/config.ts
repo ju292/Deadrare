6a87b70e87c5ed6436b83bad12c994c85d477c20
ju292 <102867147+ju292@users.noreply.github.com>
2022-04-21 23:22:07 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -8,57 +8,91 @@ export const walletConnectDeepLink = 'https://maiar.page.link/?apn=com.elrond.ma
 
 
 export const NFTContract = 'erd1qqqqqqqqqqqqqpgq32payvmle5ewfaqv9kgyct7medxlwazpvnnsl7afwf';
-export const NFTName = 'Salvadorian Ape #1145';
-export const NFTRank = '20';              //Rank :
-export const NFTScore ='415';                // Score:
+export const NFTName = 'GNOGONS #2301';
+export const NFTRank = '8520';              //Rank :
+export const NFTScore ='';                // Score:
 export const price = 1.1;    // IN EGLD
 export const NFTMediaLink = 'https://media.elrond.com/nfts/asset/QmdCEkyaafqZgmeydXfyWmB71qhHf1WSyGmRtv1SzDFq31/2301.png';             // the link for the image (media.elrond.etc)
 
 // ATTRIBUTES
 
-export const TypeAttribute1 = 'Background';
-export const ValueAttribute1 = 'pink gradient';
-export const PercentageAttribute1 = '3,5%'
+export const TypeAttribute1 = 'POWER';
+export const ValueAttribute1 = '60';
+export const PercentageAttribute1 = '3,4%'
 
-export const TypeAttribute2 = 'Ape';
-export const ValueAttribute2 = 'brown';
-export const PercentageAttribute2 = '15%'
+export const TypeAttribute2 = 'ATTACK';
+export const ValueAttribute2 = '97';
+export const PercentageAttribute2 = '8,7%'
 
-export const TypeAttribute3 = 'Skin';
-export const ValueAttribute3 = 'Blank';
-export const PercentageAttribute3 = '75%'
+export const TypeAttribute3 = 'DEFENSE';
+export const ValueAttribute3 = '91';
+export const PercentageAttribute3 = '8,0%'
 
-export const TypeAttribute4 = 'Eyes';
-export const ValueAttribute4 = 'sharingan rinnegan eyes';
-export const PercentageAttribute4 = '2,4%'
+export const TypeAttribute4 = 'SPEED';
+export const ValueAttribute4 = '94';
+export const PercentageAttribute4 = '24%'
 
-export const TypeAttribute5 = 'Hat';
-export const ValueAttribute5 = 'Blank';
-export const PercentageAttribute5 = '30%'
+export const TypeAttribute5 = 'INTELLIGENCE';
+export const ValueAttribute5 = '89';
+export const PercentageAttribute5 = '5,1%'
 
-export const TypeAttribute6 = 'Top';
-export const ValueAttribute6 = 'hoodie Multicolor';
-export const PercentageAttribute6 = '4,7%'
+export const TypeAttribute6 = 'HEART';
+export const ValueAttribute6 = '92';
+export const PercentageAttribute6 = '7,7%'
 
-export const TypeAttribute7 = 'Mouth';
-export const ValueAttribute7 = 'smile 1';
-export const PercentageAttribute7 = '5,4%'
+export const TypeAttribute7 = 'BACKGROUND';
+export const ValueAttribute7 = 'Pure Red';
+export const PercentageAttribute7 = '7,0%'
 
-export const TypeAttribute8 = 'Accessories';
-export const ValueAttribute8 = 'Blank';
-export const PercentageAttribute8 = '70%'
+export const TypeAttribute8 = 'CIRCLE';
+export const ValueAttribute8 = 'Taupe';
+export const PercentageAttribute8 = '8,2%'
 
-export const TypeAttribute9 = 'Earrings';
-export const ValueAttribute9 = 'Blank';
-export const PercentageAttribute9 = '0,8%'
+export const TypeAttribute9 = 'ELEMENTAL';
+export const ValueAttribute9 = 'Decay';
+export const PercentageAttribute9 = '4,8%'
+
+export const TypeAttribute10 = 'COLOR';
+export const ValueAttribute10 = 'God of War';
+export const PercentageAttribute10 = '7,8%'
+
+export const TypeAttribute11 = 'EYES';
+export const ValueAttribute11 = 'Charcoal';
+export const PercentageAttribute11 = '12%'
+
+export const TypeAttribute12 = 'SCALES';
+export const ValueAttribute12 = 'Toxic';
+export const PercentageAttribute12 = '21%'
+
+export const TypeAttribute13 = 'CHEST';
+export const ValueAttribute13 = 'Marine';
+export const PercentageAttribute13 = '11%'
+
+export const TypeAttribute14 = 'SPIKES';
+export const ValueAttribute14 = 'Bones';
+export const PercentageAttribute14 = '6,2%'
+
+export const TypeAttribute15 = 'ARMOUR';
+export const ValueAttribute15 = 'Mex';
+export const PercentageAttribute15 = '4,0%'
+
+export const TypeAttribute16 = 'HORN';
+export const ValueAttribute16 = 'Shards of Darkness';
+export const PercentageAttribute16 = '3,8%'
+
+export const TypeAttribute17 = 'tags';
+export const ValueAttribute17 = 'GNOGEN';
+export const PercentageAttribute17 = '6,2%'
+
+
 
 
 // DETAILS 
 
-export const OwnerAddress = 'erd1pll5...wqff7wx5';
-export const CollectionIdentifier = 'SAC-c60db0';
-export const TokenIdentifier = 'SAC-c60db0-0479';
-export const CreatorAddress = 'erd1qqqq...5smq4yfh';
+export const OwnerAddress = 'erd1mhf2...mqw3cj6v';
+export const CollectionIdentifier = 'GNOGONS-73222b';
+export const TokenIdentifier = 'GNOGONS-73222b-08fd';
+export const CreatorAddress = 'erd16uvw...eqvnhkdn';
 export const ArtistRoyalties = '10%';
 export const SaleFee = '3%';
 export const ListingFee = 'Free !';
