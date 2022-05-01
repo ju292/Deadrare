c92309f8c41dd40f8c7b3a5fc07cd7f782ea83e2
ju292 <102867147+ju292@users.noreply.github.com>
2022-05-01 22:45:20 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -11,7 +11,7 @@ export const NFTContract = 'erd1qqqqqqqqqqqqqpgq32payvmle5ewfaqv9kgyct7medxlwazp
 export const NFTName = 'Salvadorian Ape #49';
 export const NFTRank = '420';              //Rank :
 export const NFTScore ='168';                // Score:
-export const price = 3.5;    // IN EGLD
+export const price = 2;    // IN EGLD
 export const NFTMediaLink = 'https://media.elrond.com/nfts/asset/QmRkHxLNnrUYpErMxQvhZwQ3XHvF6LNPhw9AXeZDMtLrTC/696.png';             // the link for the image (media.elrond.etc)
 
 // ATTRIBUTES
