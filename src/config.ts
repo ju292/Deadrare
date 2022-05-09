c67a74067e6897c29d878ef47ed18b1eb03d260e
ju292 <102867147+ju292@users.noreply.github.com>
2022-05-09 22:38:12 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -9,36 +9,36 @@ export const walletConnectDeepLink = 'https://maiar.page.link/?apn=com.elrond.ma
 
 export const NFTContract = 'erd1qqqqqqqqqqqqqpgq32payvmle5ewfaqv9kgyct7medxlwazpvnnsl7afwf';
 export const NFTName = 'MKR1st #99';
-export const NFTRank = '2';              //Rank :
-export const NFTScore ='637';                // Score:
+export const NFTRank = '3';              //Rank :
+export const NFTScore ='611';                // Score:
 export const price = 2;    // IN EGLD
 export const NFTMediaLink = 'https://media.elrond.com/nfts/asset/QmTzNi8m7XVBuhDkmZWyEi1pawJfkdmr5D8iZGsTQwvAcA/99.mp4';             // the link for the image (media.elrond.etc)
 
 // ATTRIBUTES
 
 export const TypeAttribute1 = 'Background';
-export const ValueAttribute1 = 'Neon';
-export const PercentageAttribute1 = '16%';
+export const ValueAttribute1 = 'Desert';
+export const PercentageAttribute1 = '11%';
 
 export const TypeAttribute2 = 'Body';
-export const ValueAttribute2 = 'RoseQuartz';
-export const PercentageAttribute2 = '1.5%';
+export const ValueAttribute2 = 'Verypery Gold';
+export const PercentageAttribute2 = '2.5%';
 
 export const TypeAttribute3 = 'Chair';
-export const ValueAttribute3 = 'Gray Gold';
-export const PercentageAttribute3 = '2.9%';
+export const ValueAttribute3 = 'Gold';
+export const PercentageAttribute3 = '0.85%';
 
 export const TypeAttribute4 = 'Engine';
 export const ValueAttribute4 = 'Goldbot';
 export const PercentageAttribute4 = '1.00%';
 
 export const TypeAttribute5 = 'Floor';
-export const ValueAttribute5 = 'OrangePathsFloor';
+export const ValueAttribute5 = 'OrangepathsFloor';
 export const PercentageAttribute5 = '3.4%';
 
 export const TypeAttribute6 = 'Wheels';
-export const ValueAttribute6 = 'Rosequartz';
-export const PercentageAttribute6 = '0.25%';
+export const ValueAttribute6 = 'Gold';
+export const PercentageAttribute6 = '12%';
 
 
 
@@ -51,7 +51,7 @@ export const PercentageAttribute6 = '0.25%';
 
 // DETAILS 
 
-export const OwnerAddress = 'erd1fefv...3q74lswx';
+export const OwnerAddress = 'erd1yw39...3sglc0hr';
 export const CollectionIdentifier = 'RACE-c06f4e';
 export const TokenIdentifier = 'RACE-c06f4e-0389';
 export const CreatorAddress = 'erd1qqqq...fq6kh3ju';
