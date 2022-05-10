4c0bbebc300e2c79d12c2860a2ff1a6f95748d62
ju292 <102867147+ju292@users.noreply.github.com>
2022-05-10 20:02:51 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -8,37 +8,50 @@ export const walletConnectDeepLink = 'https://maiar.page.link/?apn=com.elrond.ma
 
 
 export const NFTContract = 'erd1qqqqqqqqqqqqqpgq32payvmle5ewfaqv9kgyct7medxlwazpvnnsl7afwf';
-export const NFTName = 'MKR1st #350';
-export const NFTRank = '6';              //Rank :
-export const NFTScore ='576';                // Score:
+export const NFTName = 'Piglet #326';
+export const NFTRank = '857';              //Rank :
+export const NFTScore ='32';                // Score:
 export const price = 2;    // IN EGLD
 export const NFTMediaLink = 'https://media.elrond.com/nfts/asset/QmTzNi8m7XVBuhDkmZWyEi1pawJfkdmr5D8iZGsTQwvAcA/350.mp4';             // the link for the image (media.elrond.etc)
 
 // ATTRIBUTES
 
-export const TypeAttribute1 = 'Background';
-export const ValueAttribute1 = 'Desert';
-export const PercentageAttribute1 = '11%';
+export const TypeAttribute1 = 'background';
+export const ValueAttribute1 = 'black';
+export const PercentageAttribute1 = '25%';
 
-export const TypeAttribute2 = 'Body';
-export const ValueAttribute2 = 'Verypery Gold';
-export const PercentageAttribute2 = '2.5%';
+export const TypeAttribute2 = 'body';
+export const ValueAttribute2 = 'body';
+export const PercentageAttribute2 = '100%';
 
-export const TypeAttribute3 = 'Chair';
-export const ValueAttribute3 = 'Gold';
-export const PercentageAttribute3 = '0.85%';
+export const TypeAttribute3 = 'shirt';
+export const ValueAttribute3 = 'prison shirt';
+export const PercentageAttribute3 = '28%';
 
-export const TypeAttribute4 = 'Engine';
-export const ValueAttribute4 = 'Goldbot';
-export const PercentageAttribute4 = '1.00%';
+export const TypeAttribute4 = 'pant';
+export const ValueAttribute4 = 'diaper';
+export const PercentageAttribute4 = '24%';
 
-export const TypeAttribute5 = 'Floor';
-export const ValueAttribute5 = 'OrangepathsFloor';
-export const PercentageAttribute5 = '3.4%';
+export const TypeAttribute5 = 'chain';
+export const ValueAttribute5 = 'no chain';
+export const PercentageAttribute5 = '35%';
 
-export const TypeAttribute6 = 'Wheels';
-export const ValueAttribute6 = 'Gold';
-export const PercentageAttribute6 = '12%';
+export const TypeAttribute6 = 'eye';
+export const ValueAttribute6 = 'High Eyes';
+export const PercentageAttribute6 = '28%';
+
+export const TypeAttribute7 = 'mouth';
+export const ValueAttribute7 = 'tongue normal';
+export const PercentageAttribute7 = '21%';
+
+export const TypeAttribute8 = 'sunglass';
+export const ValueAttribute8 = 'Rainbow Glasses';
+export const PercentageAttribute8 = '25%';
+
+export const TypeAttribute9 = 'shoe';
+export const ValueAttribute9 = 'Hogye West Shoes Blacks';
+export const PercentageAttribute9 = '27%';
+
 
 
 
@@ -51,12 +64,12 @@ export const PercentageAttribute6 = '12%';
 
 // DETAILS 
 
-export const OwnerAddress = 'erd1yw39...3sglc0hr';
-export const CollectionIdentifier = 'RACE-c06f4e';
-export const TokenIdentifier = 'RACE-c06f4e-0389';
-export const CreatorAddress = 'erd1qqqq...fq6kh3ju';
+export const OwnerAddress = 'erd12xun...hs3jyhjt';
+export const CollectionIdentifier = 'PIGLETHH-7357ca;
+export const TokenIdentifier = 'PIGLETHH-7357ca-0146';
+export const CreatorAddress = 'erd1qqp6...rqk2pcss';
 export const ArtistRoyalties = '10%';
-export const SaleFee = '3%';
+export const SaleFee = '5%';
 export const ListingFee = 'Free !';
 
 
