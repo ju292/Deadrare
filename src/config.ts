97d13a6022bd09737e77dc2b105daacd47837147
ju292 <102867147+ju292@users.noreply.github.com>
2022-12-26 22:52:03 +0100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -8,8 +8,8 @@ export const walletConnectDeepLink = 'https://maiar.page.link/?apn=com.elrond.ma
 
 
 export const NFTContract = 'erd1qqqqqqqqqqqqqpgq32payvmle5ewfaqv9kgyct7medxlwazpvnnsl7afwf';
-export const NFTName = 'BAXC #6196';
-export const NFTRank = '347';              //Rank :
+export const NFTName = 'BAXC #5045';
+export const NFTRank = '577';              //Rank :
 export const NFTScore ='95';                // Score:
 export const price = '8.5';    // IN EGLD
 export const NFTMediaLink = 'https://media.elrond.com/nfts/asset/Qmf9DdY4tzgU3JEBe8TE88X2uD5pJfAYZRdLvXruRiF1uw/4809.png';             // the link for the image (media.elrond.etc)
@@ -21,15 +21,15 @@ export const ValueAttribute1 = 'LeatherX';
 export const PercentageAttribute1 = '0.30%';
 
 export const TypeAttribute2 = 'Eyes';
-export const ValueAttribute2 = 'BoredAviator';
+export const ValueAttribute2 = 'Reddish';
 export const PercentageAttribute2 = '3.46%';
 
 export const TypeAttribute3 = 'Mouth';
-export const ValueAttribute3 = 'SayL';
+export const ValueAttribute3 = 'Rage';
 export const PercentageAttribute3 = '3.46%';
 
 export const TypeAttribute4 = 'Fur';
-export const ValueAttribute4 = 'Sky';
+export const ValueAttribute4 = 'Red';
 export const PercentageAttribute4 = '8.38%';
 
 export const TypeAttribute5 = 'Background';
@@ -37,8 +37,12 @@ export const ValueAttribute5 = 'Melon';
 export const PercentageAttribute5 = '12.29%';
 
 export const TypeAttribute6 = 'Earrings';
-export const ValueAttribute6 = 'PearlStrass';
+export const ValueAttribute6 = 'SilverlStrass';
 export const PercentageAttribute6 = '12.34%';
+
+export const TypeAttribute6 = 'Hat';
+export const ValueAttribute6 = 'WinterHat';
+export const PercentageAttribute6 = '2.71%';
 
 
 
@@ -54,9 +58,9 @@ export const PercentageAttribute6 = '12.34%';
 
 // DETAILS 
 
-export const OwnerAddress = 'erd106chdu3wknj8d743ktezsr2f262vvaf3qd88xkn68ysd86dwxqkskhs2qp';
+export const OwnerAddress = 'erd1gtzlvxxth07cdm7wxqnurrln63926z9n4j6x6q6vkzerhw0jwkhqedx07r';
 export const CollectionIdentifier = 'BAXC-cdf74d';
-export const TokenIdentifier = 'BAXC-cdf74d-Oc6d';
+export const TokenIdentifier = 'BAXC-cdf74d-13b5';
 export const CreatorAddress = 'erd1qqqqqqqqqqqqqpgqengxksed64chl3zrxc5fjl97cz9nunmkys5sxf69aa';
 export const ArtistRoyalties = '10%';
 export const SaleFee = '2%';
