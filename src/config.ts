4b7481fc69a01fc15fb7c4a762446d2437a05708
ju292 <102867147+ju292@users.noreply.github.com>
2023-01-01 20:39:28 +0100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -8,76 +8,42 @@ export const walletConnectDeepLink = 'https://maiar.page.link/?apn=com.elrond.ma
 
 
 export const NFTContract = 'erd1qqqqqqqqqqqqqpgq32payvmle5ewfaqv9kgyct7medxlwazpvnnsl7afwf';
-export const NFTName = 'GenesisSpaceApe #704';
-export const NFTRank = '1270';              //Rank :
+export const NFTName = 'BAXC #9213';
+export const NFTRank = '200';              //Rank :
 export const NFTScore ='95';                // Score:
-export const price = '14.5';    // IN EGLD
-export const NFTMediaLink = 'https://media.elrond.com/nfts/asset/bafybeicsa2ygmod3btvjtdvf6pofhp4vtjgfzgpmjhmgiryunrceccxmee/704.png';             // the link for the image (media.elrond.etc)
+export const price = '20';    // IN EGLD
+export const NFTMediaLink = 'https://media.elrond.com/nfts/asset/Qmf9DdY4tzgU3JEBe8TE88X2uD5pJfAYZRdLvXruRiF1uw/1964.png';             // the link for the image (media.elrond.etc)
 
 // ATTRIBUTES
 
-export const TypeAttribute1 = 'Species';
-export const ValueAttribute1 = 'Genesis Space Ape';
+export const TypeAttribute1 = 'Clothes';
+export const ValueAttribute1 = 'XHoodie';
 export const PercentageAttribute1 = '6.1%';
 
-export const TypeAttribute2 = 'Rarity Class';
-export const ValueAttribute2 = 'Legendary';
+export const TypeAttribute2 = 'Earrings';
+export const ValueAttribute2 = 'PearlChain';
 export const PercentageAttribute2 = '0.32%';
 
-export const TypeAttribute3 = 'Head';
-export const ValueAttribute3 = 'Default';
-export const PercentageAttribute3 = '6.1%';
+export const TypeAttribute3 = 'Eyes';
+export const ValueAttribute3 = 'Depressed';
+export const PercentageAttribute3 = '1.98%';
 
-export const TypeAttribute4 = 'Face';
-export const ValueAttribute4 = 'None';
-export const PercentageAttribute4 = '6.1%';
+export const TypeAttribute4 = 'Fur';
+export const ValueAttribute4 = 'Purple';
+export const PercentageAttribute4 = '1.82%';
 
-export const TypeAttribute5 = 'Skin';
-export const ValueAttribute5 = 'Muerto';
-export const PercentageAttribute5 = '0.09%';
+export const TypeAttribute5 = 'Background';
+export const ValueAttribute5 = 'Peach';
+export const PercentageAttribute5 = '11.26%';
 
-export const TypeAttribute6 = 'Eyes';
-export const ValueAttribute6 = 'Angry';
-export const PercentageAttribute6 = '0.62%';
+export const TypeAttribute6 = 'Hat';
+export const ValueAttribute6 = 'TheKing';
+export const PercentageAttribute6 = '0.19%';
 
 export const TypeAttribute7 = 'Mouth';
-export const ValueAttribute7 = 'Jovial';
-export const PercentageAttribute7 = '0.36%';
+export const ValueAttribute7 = 'BoredTongue';
+export const PercentageAttribute7 = '1.76%';
 
-export const TypeAttribute8 = 'Earrings';
-export const ValueAttribute8 = 'Gold Cross';
-export const PercentageAttribute8 = '0.27%';
-
-export const TypeAttribute9 = 'Headgear';
-export const ValueAttribute9 = 'Fluffy Hat';
-export const PercentageAttribute9 = '0.02%';
-
-
-
-export const TypeAttribute11 = 'Body';
-export const ValueAttribute11 = 'Rogue Armor';
-export const PercentageAttribute11 = '0.03%';
-
-export const TypeAttribute12 = 'Legs';
-export const ValueAttribute12 = 'Pilot Pants Green';
-export const PercentageAttribute12 = '0.16%';
-
-export const TypeAttribute13 = 'LegAccessories';
-export const ValueAttribute13 = 'Waistband Orange';
-export const PercentageAttribute13 = '0.02%';
-
-export const TypeAttribute14 = 'Background';
-export const ValueAttribute14 = 'Neon';
-export const PercentageAttribute14 = '0.32%';
-
-export const TypeAttribute15 = 'Perk 1';
-export const ValueAttribute15 = 'Black Widow';
-export const PercentageAttribute15 = '0.24%';
-
-
-export const TypeAttribute16 = 'Perk 2';
-export const ValueAttribute16 = 'Elemental Weapons Proficiency';
-export const PercentageAttribute16 = '0.18%';
 
 
 
@@ -92,11 +58,11 @@ export const PercentageAttribute16 = '0.18%';
 
 // DETAILS 
 
-export const OwnerAddress = 'erd160p9vp5lnjf7dvur0hal6mggdl25vw0qr3fpn8edsew30q7dhu5q442fwm';
+export const OwnerAddress = 'erd1h873wrc6x2vzrvhke58350y4sua0r5eglhn5vhdg929uy662lalq95z9se';
 export const CollectionIdentifier = 'GSPACEAPE-08bc2b';
-export const TokenIdentifier = 'GSPACEAPE-08bc2b-15e7';
+export const TokenIdentifier = 'BAXC-cdf74d-23fd';
 export const CreatorAddress = 'erd1qqqqqqqqqqqqqpgqengxksed64chl3zrxc5fjl97cz9nunmkys5sxf69aa';
-export const ArtistRoyalties = '5%';
+export const ArtistRoyalties = '10%';
 export const SaleFee = '2%';
 export const ListingFee = 'Free !';
 
