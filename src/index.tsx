6f0e1df1b30ead3731cf2a02f3c5e393f28d8be8
ju292 <102867147+ju292@users.noreply.github.com>
2023-06-19 22:13:34 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,59 +1,54 @@
-<!DOCTYPE html>
-<html lang="en" >
-  <head>
-    <meta charset="utf-8" />
-    <!-- <link rel="icon" href="%PUBLIC_URL%/favicon.ico" /> -->
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="theme-color" content="#000000" />
-    <meta
-      name="description"
-      content="Web site created using create-react-app"
-    />
-    <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
-    <!--
-      manifest.json provides metadata used when your web app is installed on a
-      user's mobile device or desktop. See https://developers.google.com/web/fundamentals/web-app-manifest/
-    -->
-    <link
-    rel="icon"
-    type="image/png"
-    sizes="32x32"
-    href="img/favicon-32x32.png"
-  />
-  <link
-    href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
-    rel="stylesheet"
-  />
-    <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
-    <link rel="stylesheet" href="./style.css" />
-    <style>
-      body {
-        color: black;
-      }
-    </style>
-    <!--
-      Notice the use of %PUBLIC_URL% in the tags above.
-      It will be replaced with the URL of the `public` folder during the build.
-      Only files inside the `public` folder can be referenced from the HTML.
+import React from 'react';
+import { Pane, Heading, Link, Code, Text } from 'evergreen-ui';
+import ReactDOM from 'react-dom';
+import { NetworkConfig, ProxyProvider } from '@elrondnetwork/erdjs';
+import App from './App';
+import { network } from './config';
+import reportWebVitals from './reportWebVitals';
+import './main.css';
 
-      Unlike "/favicon.ico" or "favicon.ico", "%PUBLIC_URL%/favicon.ico" will
-      work correctly both with client-side routing and a non-root public URL.
-      Learn how to configure a non-root public URL by running `npm run build`.
-    -->
-    <title>React App</title>
-  </head>
-  <body bgcolor="black" >
-    <noscript>You need to enable JavaScript to run this app.</noscript>
-    <div id="root"></div>
-    <!--
-      This HTML file is a template.
-      If you open it directly in the browser, you will see an empty page.
+let MainComponent = (
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-      You can add webfonts, meta tags, or analytics to this file.
-      The build step will place the bundled scripts into the <body> tag.
+// Sync Elrond provider
+if (network?.gatewayAddress) {
+  const proxyProvider = new ProxyProvider(network.gatewayAddress);
+  NetworkConfig.getDefault().sync(proxyProvider);
+} else {
+  MainComponent = (
+    <React.StrictMode>
+      <Pane textAlign="center" paddingTop={100}>
+        <Heading size={900} marginBottom={30}>
+          Elrond Dapp starter kit
+        </Heading>
+        <Heading>
+          Please configure .env.local file to be able to run the app.
+        </Heading>
+        <Pane marginTop={20} marginBottom={20}>
+          <Code>cp .env.devnet.example .env.local</Code>
+          <Text> or </Text>
+          <Code>cp .env.testnet.example .env.local</Code>
+        </Pane>
+        <Heading size={300}>
+          More info in the{' '}
+          <Link href="">
+            README.md
+          </Link>{' '}
+          file.
+        </Heading>
+      </Pane>
+    </React.StrictMode>
+   
+  );
+}
 
-      To begin the development, run `npm start` or `yarn start`.
-      To create a production bundle, use `npm run build` or `yarn build`.
-    -->
-  </body>
-</html>
+ReactDOM.render(MainComponent, document.getElementById('root'));
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
